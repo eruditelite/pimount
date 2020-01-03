56d436dfc544854f06e7315dea1ed58c7e67ff4a
John Jacques <eruditelite@gmail.com>
2020-01-03 12:28:27 -0600

    Add a Line for RA Tracking in output.sh

--- tests/output.sh
+++ tests/output.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-output -p 26:19:13:6:5 -r 3 -d 1 -w 10000 -e 20000 -s 100
+SCRIPTPATH="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
+
+#sudo "${SCRIPTPATH}/output" -p 27:17:4:3:2 18 -r 0 -d 0 -w 500 -e 10000 -s 1000
+
+## RA Tracking: Not Perfect but Close
+sudo "${SCRIPTPATH}/output" -p 26:19:13:6:5 -r 3 -d 0 -w 500 -e 12100 -s 0
