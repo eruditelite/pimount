6bc0fc08d46b8918f98b1f2aae89b6d33f660e6e
John Jacques <eruditelite@gmail.com>
2020-01-15 17:18:22 -0600

    Updated RA Tracking Test
    See tests/output.sh...  After cleaning up the timing
    and delay, make output match the old settings.

--- tests/output.sh
+++ tests/output.sh
@@ -5,4 +5,8 @@ SCRIPTPATH="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
 sudo "${SCRIPTPATH}/output" -p 26:19:13:6:5 -r 3 -d 0 -w 20000 -e 20000 -s 200
 
 ## RA Tracking: Not Perfect but Close
-#sudo "${SCRIPTPATH}/output" -p 26:19:13:6:5 -r 3 -d 0 -w 500 -e 12400 -s 0
+##
+## Update to match the old tracking (Moon on Jan 10 2020) after cleaning up
+## the timing.
+##
+#sudo "${SCRIPTPATH}/output" -p 26:19:13:6:5 -r 3 -d 0 -w 572 -e 12475 -s 0
